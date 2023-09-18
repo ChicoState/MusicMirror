84d4de583b196037cfed8bfded0a19ba37648bb5
khasek <kmhasek@gmail.com>
2023-09-18 14:37:11 -0700

    basic page for viewing a playlist

--- musicmirror/src/App.js
+++ musicmirror/src/App.js
@@ -5,19 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MusicMirror</h1>
       </header>
+      <main className="App-main">
+        <h2 className="Test-h2">Streaming Service</h2>
+        <div className="Test-playlist">
+          <h3>Playlist</h3>
+          <ol>
+              <li>song</li>
+              <li>song</li>
+              <li>song</li>
+              <li>song</li>
+              <li>song with a long title</li>
+              <li>song</li>
+            </ol>
+        </div>
+      </main>
     </div>
   );
 }
