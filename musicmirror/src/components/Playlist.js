beb0d02b6a356e1dbb80bde40db8c9ed8f19f6eb
khasek <kmhasek@gmail.com>
2023-09-25 14:19:47 -0700

    added Bootstrap
    The latest version of Bootstrap has been added via CDN, so nobody should have to install anything. (Note: This commit also includes a slight change in the way background color is handled, unrelated to Bootstrap.)

--- musicmirror/src/components/Playlist.js
+++ musicmirror/src/components/Playlist.js
@@ -7,9 +7,9 @@ function Playlist() {
       <h2 className="service-name">Streaming Service</h2>
       <div className="list-container">
         <h3>{playlist.title}</h3>
-        <ol>
+        <ol className="">
             {playlist.songs.map((song) => (
-              <li>{song.title} | {song.artist} | {song.album} | {song.length}</li>
+              <li className="">{song.title} | {song.artist} | {song.album} | {song.length}</li>
             ))}
           </ol>
       </div>
