39ef3e5f5dc87101aab8d0ffe2cd0f8008fa0e31
akachriss <chrisarobles27@gmail.com>
2023-10-05 16:38:01 -0700

    SpotProf & Playlist in classes

--- musicmirror/src/components/SpotifyProfile.js
+++ musicmirror/src/components/SpotifyProfile.js
@@ -1,9 +1,14 @@
 import * as auth from '../auth';
 import '../styles/SpotifyProfile.css';
+import React from 'react';
 
-export function SpotifyProfile() {
-  return (
-    <section id="profile">
+class SpotifyProfile extends React.Component {
+
+  //Possible constructor with super()
+
+  render() {
+    return (
+      <section id="profile">
       <h3 className="d-flex justify-content-center">Logged into Spotify as&nbsp;<div id="displayName"></div></h3>
       <ul>
         <li className="d-flex justify-content-between">User ID:&nbsp;<div id="id" className="profile-info"></div></li>
@@ -12,7 +17,24 @@ export function SpotifyProfile() {
       </ul>
       <button className="btn btn-secondary" onClick={auth.checkCode}>Sign in to Spotify</button>
     </section>
-  );
+    );
+  }
 }
 
 export default SpotifyProfile;
+
+/*
+export function SpotifyProfile() {
+  return (
+    <section id="profile">
+      <h3 className="d-flex justify-content-center">Logged into Spotify as&nbsp;<div id="displayName"></div></h3>
+      <ul>
+        <li className="d-flex justify-content-between">User ID:&nbsp;<div id="id" className="profile-info"></div></li>
+        <li className="d-flex justify-content-between">Email:&nbsp;<div id="email" className="profile-info"></div></li>
+        //{ <li className="d-flex justify-content-between">Profile Image:&nbsp;<div id="imgUrl"></div></li> }
+      </ul>
+      <button className="btn btn-secondary" onClick={auth.checkCode}>Sign in to Spotify</button>
+    </section>
+  );
+}
+*/
