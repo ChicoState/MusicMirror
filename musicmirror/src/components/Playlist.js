53aa702a0b32126c5fd884929bba78755a4ba4a2
khasek <kmhasek@gmail.com>
2023-10-11 14:47:29 -0700

    started styling

--- musicmirror/src/components/Playlist.js
+++ musicmirror/src/components/Playlist.js
@@ -22,24 +22,22 @@ class Playlist extends React.Component{
       return (
         <div className="Playlist-container mt-5">
           <div className="list-container">
-            <h3>Add songs to preview playlist</h3>
+            <h1>Add songs to preview playlist</h1>
           </div>
         </div>
       );
     } else if (this.props.list.songs && Object.keys(this.props.list.songs).length > 0) {
       return (
-        <div className="Playlist-container mt-5">
-          <h3>{this.props.list.title}</h3>
+        <div className="Playlist-container mt-5 py-2">
+          <h1>{this.props.list.title}</h1>
 
           {/* List of song cards */}
           {this.props.list.songs.map((song) => (
-            <div className="song-card">
-              <div className="song-preview d-flex">
-                <img src="./images/play-circle.svg" alt="play" className="play-button" onClick="" role="button"/> 
-                <div className="details flex-grow-1" role="button" onClick={() => this.handleSongSelection(song)} data-bs-toggle="modal" data-bs-target="#song-details">
-                  <h4>{song.title}</h4>
-                  <p>{song.artist}</p>
-                </div>
+            <div className="my-1 song-card d-flex">
+              <img src="./images/play-circle.svg" alt="play" className="px-2 py-1 play-button" onClick="" role="button"/> 
+              <div className="p-1 details flex-grow-1" role="button" onClick={() => this.handleSongSelection(song)} data-bs-toggle="modal" data-bs-target="#song-details">
+                <h2 className="m-0">{song.title}</h2>
+                <p className="m-0">{song.artist}</p>
               </div>
             </div>
           ))}
@@ -49,7 +47,7 @@ class Playlist extends React.Component{
             <SongDetailsModal song={this.state.selectedSong}/>
           </div>
 
-          <button className="btn btn-secondary" onClick={() => genPlaylist(this.props.list)}>Confirm Playlist</button>
+          <button className="m-2 px-5 btn btn-secondary" onClick={() => genPlaylist(this.props.list)}>Confirm Playlist</button>
         </div>
       );
 
--- musicmirror/src/components/SongDetailsModal.js
+++ musicmirror/src/components/SongDetailsModal.js
@@ -28,7 +28,11 @@ class SongDetailsModal extends React.Component{
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="searchbox input-group">
+              <input className="form-control input-group-text" id="textInput" type="search" aria-label="refine search" onChange={this.handleChange} value={this.message} placeholder="" />
+              <img src="./images/search.svg" alt="search" className="btn btn-info" onClick={this.handleSubmit} role="button"/>
+            </div>
+            <button className="ms-2 btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
             <div className="modal-card">
@@ -40,19 +44,10 @@ class SongDetailsModal extends React.Component{
               <p>Artist: {song.artist}</p>
               <p>Album: {song.album}</p>
             </div>
-            <div className="search-container">
-              <label className="form-label" htmlFor="textInput">
-                <h5 className="mt-5 mb-0">Refine Search:</h5>
-              </label>
-              <div className="searchbox input-group">
-                <input className="form-control input-group-text" id="textInput" type="text" onChange={this.handleChange} value={this.message} placeholder="" />
-                <img src="./images/search.svg" alt="search" className="btn btn-info" onClick={this.handleSubmit} role="button"/>
-              </div>
-            </div>
           </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-success" data-bs-dismiss="modal">Confirm Selection</button>
-            <button type="button" className="btn btn-danger">Remove Song</button>
+          <div className="modal-footer d-flex">
+            <button type="button" className="btn btn-success flex-fill" data-bs-dismiss="modal">Confirm Selection</button>
+            <button type="button" className="btn btn-danger flex-fill">Remove Song</button>
           </div>
         </div>
       </div>
