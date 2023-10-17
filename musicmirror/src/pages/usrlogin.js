dc555d9188495df2a05cfa07329edff70634f93c
Angel086 <aguerra688@gmail.com>
2023-10-17 13:32:12 -0700

    Burger menu works, issue with display of home link

--- musicmirror/src/pages/usrlogin.js
+++ musicmirror/src/pages/usrlogin.js
@@ -1,7 +1,13 @@
+import SpotifyProfile from "../components/SpotifyProfile";
+import Playlist from "../components/Playlist";
+import Youtube from "./youtube";
+import apple from "./apple";
+import Home from "./home";
 export default function  usrlogin(){
     return(
         <dive>
-            <h1>uSERS</h1>
+            <h1>uSERS LOGINGS go here</h1>
+
         </dive>
     );
 }
