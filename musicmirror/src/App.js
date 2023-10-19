a1861f26c71eedb4bae45b470c20803238f305c5
Angel086 <aguerra688@gmail.com>
2023-10-19 10:16:36 -0700

    Have tabs working, For some reason it didn't upload yesterday even though i pushed

--- musicmirror/src/App.js
+++ musicmirror/src/App.js
@@ -4,7 +4,7 @@ import Playlist from './components/Playlist';
 import SpotifyProfile from './components/SpotifyProfile';
 import { useState } from 'react';
 import { findSongs } from './playlist';
-
+import {Tabs, Tab} from 'react-bootstrap';
 function App() {
   const [list, setList] = useState();
   
@@ -17,15 +17,33 @@ function App() {
       <header className="App-header d-flex justify-content-center align-items-center">
         <h1>MusicMirror</h1>        
       </header>
-      <main className="App-main mx-0 px-5 py-4 container">
-        <div className="row grid gap-5">
-          <AddSongs handleMsg={handleMsg}/>
-          <div className="spotify-stuff p-4 col-12 col-md">
-            <SpotifyProfile />
-            <Playlist list={list}/>
-          </div>
-        </div>
-      </main>
+
+
+
+      <div className="App-main mx-0 px-5 py-4 container">
+        <Tabs defaultAct="addSongs" id="tab">
+          <Tab eventKey={"add Songs"} title="Add Songs">
+            <AddSongs handleMsg={handleMsg}/>
+          </Tab>
+          <Tab eventKey={"spotify"} title ="Spotify">
+            <div className="spotify-stuff p-4 col-12 col-md">
+              <SpotifyProfile />
+              <Playlist list={list}/>
+            </div> 
+          </Tab>
+          <Tab eventKey="youtube" title="Youtube">
+              <div className='Youtube'>
+                <h1>
+                  Signing into Youtube
+                </h1>
+                  <button > Signing</button>
+              </div>
+          </Tab>
+        </Tabs>
+      </div>
+
+
+
       <footer className="App-footer"></footer>
     </div>
   );
