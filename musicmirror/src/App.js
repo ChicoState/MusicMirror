2e90b509c93d0a6aa77b36a1c04968c5561dda62
Angel086 <aguerra688@gmail.com>
2023-10-25 13:17:45 -0700

    Hold

--- musicmirror/src/App.js
+++ musicmirror/src/App.js
@@ -13,39 +13,94 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header d-flex justify-content-center align-items-center">
-        <h1>MusicMirror</h1>        
-      </header>
+  <div className="App">
+    <header className="App-header d-flex justify-content-center align-items-center">
+      <h1>MusicMirror</h1>        
+    </header>
+      {/*  <//"App-main mx-0 px-5 py-4 container"> */}
+    <div className= "App-main mx-0 px-5 py-4 container">
+      <div className='row grid gap-5'>
 
-
-
-      <div className="App-main mx-0 px-5 py-4 container">
         <Tabs defaultAct="addSongs" id="tab">
-          <Tab eventKey={"add Songs"} title="Add Songs">
-            <AddSongs handleMsg={handleMsg}/>
-          </Tab>
-          <Tab eventKey={"spotify"} title ="Spotify">
-            <div className="spotify-stuff p-4 col-12 col-md">
-              <SpotifyProfile />
-              <Playlist list={list}/>
-            </div> 
-          </Tab>
-          <Tab eventKey="youtube" title="Youtube">
-              <div className='Youtube'>
-                <h1>
-                  Signing into Youtube
-                </h1>
-                  <button > Signing</button>
-              </div>
-          </Tab>
+            <Tab eventKey={"add Songs"} title="Add Songs">
+              <AddSongs handleMsg={handleMsg}/>
+            </Tab>
+            <Tab eventKey={"spotify"} title ="Spotify">
+              <div className="spotify-stuff p-4 col-12 col-md">
+                <SpotifyProfile />
+                <Playlist list={list}/>
+              </div> 
+            </Tab>
+            <Tab eventKey="youtube" title="Youtube">
+                <div className='Youtube'>
+                  <h1>
+                    Signing into Youtube
+                  </h1>
+                    <button > Signing</button>
+                </div>
+            </Tab>
         </Tabs>
-      </div>
+      {/**-------------------------------------------- */}
+        <Tabs className='p-4 col-12 col-md' defaultAct="addSongs" id="tab">
+            <Tab eventKey={"add Songs"} title="Add Songs">
+              <AddSongs handleMsg={handleMsg}/>
+            </Tab>
+            <Tab eventKey={"spotify"} title ="Spotify">
+              <div className="spotify-stuff p-4 col-12 col-md">
+                <SpotifyProfile />
+                <Playlist list={list}/>
+              </div> 
+            </Tab>
+            <Tab eventKey="youtube" title="Youtube">
+                <div className='Youtube'>
+                  <h1>
+                    Signing into Youtube
+                  </h1>
+                    <button > Signing</button>
+                </div>
+            </Tab>
+        </Tabs>
 
 
 
-      <footer className="App-footer"></footer>
+
+
+
+
+
+
+
+      </div> 
     </div>
+
+    <footer className="App-footer"></footer>
+
+    <div className='App-main mx-0 my-5 px-5 py-4 container'>
+      <div className='col-auto'>
+            <Tabs defaultAct="addSongs" id="tab">
+                <Tab eventKey={"add Songs"} title="Add Songs">
+                  <AddSongs handleMsg={handleMsg}/>
+                </Tab>
+                <Tab eventKey={"spotify"} title ="Spotify">
+                  <div className="spotify-stuff p-4 col-12 col-md">
+                    <SpotifyProfile />
+                    <Playlist list={list}/>
+                  </div> 
+                </Tab>
+                <Tab eventKey="youtube" title="Youtube">
+                    <div className='Youtube'>
+                      <h1>
+                        Signing into Youtube
+                      </h1>
+                        <button > Signing</button>
+                    </div>
+                </Tab>
+              </Tabs>
+        </div>
+    </div>
+
+  </div>
+    
   );
 }
 
