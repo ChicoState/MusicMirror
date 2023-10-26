97724de054eba041ec4f031a4b2febcee53f0111
khasek <kmhasek@gmail.com>
2023-10-25 22:03:55 -0700

    confirm/delete buttons functional

--- musicmirror/src/components/Playlist.js
+++ musicmirror/src/components/Playlist.js
@@ -9,12 +9,31 @@ class Playlist extends React.Component{
     this.state = {
       playlist: {},
       selectedSong: {},
+      selectedIndex: null,
     };
   }
 
-  handleSongSelection = (song) => {
+  handleSongSelection = (song, index) => {
     this.setState({selectedSong: song});
+    this.setState({selectedIndex: index});
     console.log(`selected song: ${song.tracks[0].title}`);
+  };
+
+  handleUpdate = (updatedSong) => {
+    let newList = this.props.list;
+
+    /* If passed nothing, remove the currently selected song */
+    if (!updatedSong) {
+      newList.songs.splice(this.state.selectedIndex, 1);
+      this.setState({selectedSong: {}});
+      this.setState({selectedIndex: null});
+
+    /* If passed a song, replace the currently selected song */
+    } else {
+      console.log(updatedSong);
+      newList.songs[this.state.selectedIndex] = updatedSong;
+      this.setState({selectedSong: updatedSong});
+    }
   };
 
   render() {
@@ -30,12 +49,12 @@ class Playlist extends React.Component{
           <h1>{this.props.list.title}</h1>
 
           {/* List of song cards */}
-          {this.props.list.songs.map((song) => (
+          {this.props.list.songs.map((song, index) => (
             <div className="my-1 song-card d-flex">
               {/* This img is where the song preview play/pause button 
               should go. Still needs input, handler, and formatting. */}
               <img src="./images/play-circle.svg" alt="play" className="px-2 py-1 play-button" onClick="" role="button"/> 
-              <div className="p-1 details flex-grow-1" role="button" onClick={() => this.handleSongSelection(song)} data-bs-toggle="modal" data-bs-target="#song-details">
+              <div className="p-1 details flex-grow-1" role="button" onClick={() => this.handleSongSelection(song, index)} data-bs-toggle="modal" data-bs-target="#song-details">
                 <h2 className="m-0">{song.tracks[0].title}</h2>
                 <p className="m-0">{song.tracks[0].artist}</p>
               </div>
@@ -43,9 +62,7 @@ class Playlist extends React.Component{
           ))}
 
           {/* This modal pops up when you click on a song */}
-          <div id="song-details" className="modal fade" tabindex="-1" aria-hidden="true">
-            <SongDetailsModal song={this.state.selectedSong}/>
-          </div>
+          <SongDetailsModal song={this.state.selectedSong} updatePlaylist={this.handleUpdate}/>
 
           <button className="m-2 px-5 btn btn-secondary" onClick={() => genPlaylist(this.props.list)}>Confirm Playlist</button>
         </div>
--- musicmirror/src/components/SongDetailsModal.js
+++ musicmirror/src/components/SongDetailsModal.js
@@ -6,7 +6,16 @@ class SongDetailsModal extends React.Component{
     super();
     this.state = {
       message: '',
+      trackIndex: 0,
     };
+  }
+
+  componentDidUpdate(prevProps) {
+
+    /* Make sure the trackIndex resets whenever a new song is passed */
+    if (prevProps.song !== this.props.song) {
+      this.setState({trackIndex: 0});
+    }
   }
 
   handleChange = event => {
@@ -16,71 +25,112 @@ class SongDetailsModal extends React.Component{
   };
 
   handleSubmit = async() => {
-    this.props.handleMsg(this.state.message);
     console.log(`passing: ${this.state.message}`)
+  };
+
+  handleTrackIter = (index) => {
+    if (index === "next" && this.state.trackIndex === this.props.song.tracks.length-1) {
+      this.setState({trackIndex: 0});
+    } else if (index === "next") {
+      this.setState({trackIndex: this.state.trackIndex+1});
+    } else if (index === "prev" && this.state.trackIndex === 0) {
+      this.setState({trackIndex: this.props.song.tracks.length-1});
+    } else if (index === "prev") {
+      this.setState({trackIndex: this.state.trackIndex-1});
+    } else {
+      this.setState({trackIndex: index});
+    }
+  }
+
+  handleConfirm = () => {
+    if (this.state.trackIndex !== 0) {
+      let chosenTrack = this.props.song.tracks[this.state.trackIndex];
+      this.props.song.tracks = [
+        chosenTrack,
+        ...this.props.song.tracks.slice(0, this.state.trackIndex),
+        ...this.props.song.tracks.slice(this.state.trackIndex + 1)
+      ];
+    } 
+    this.props.updatePlaylist(this.props.song);
   };
 
   render() {
 
     const song = this.props.song;
 
+    if (!song) {
+      return (
+        <div id="song-details" className="modal fade" tabindex="-1" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header"></div>
+              <div className="modal-body"></div>
+              <div className="modal-footer"></div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
     return (
-      <div className="modal-dialog modal-lg modal-fullscreen-sm-down">
-        <div className="modal-content">
-          <div className="modal-header">
-            <div className="searchbox input-group">
-              <input className="form-control input-group-text" id="textInput" type="search" aria-label="refine search" placeholder={song.query} onChange={this.handleChange} value={this.message} />
-              <img src="./images/search.svg" alt="search" className="btn btn-info" onClick={this.handleSubmit} role="button"/>
+      <div id="song-details" className="modal fade" tabindex="-1" aria-hidden="true">
+        <div className="modal-dialog modal-lg modal-fullscreen-sm-down">
+          <div className="modal-content">
+            <div className="modal-header">
+              <div className="searchbox input-group">
+                <input className="form-control input-group-text" id="textInput" type="search" aria-label="refine search" placeholder={song.query} onChange={this.handleChange} value={this.message} />
+                <img src="./images/search.svg" alt="search" className="btn btn-info" onClick={this.handleSubmit} role="button"/>
+              </div>
+              <button className="ms-2 btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <button className="ms-2 btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
 
-          {/* This section handles search result slides */}
-          <div id="details-carousel" className="modal-body carousel slide">
-            <div className="carousel-indicators">
-              {song.tracks && song.tracks.map((track, index) => (
-                <button type="button" data-bs-target="#details-carousel" data-bs-slide-to={index} className={index === 0 ? "active" : ""} aria-current={index === 0 ? "true" : ""} aria-label={`Search Result ${index+1}`}></button>
-              ))}
-            </div>
-            <div className="carousel-inner">
-              {song.tracks && song.tracks.map((track, index) => (
-                <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
-                  <div className="mb-2 details-header d-flex align-items-center">
-                    {/* This img is where the song preview play/pause button 
-                    should go. Still needs input, handler, and formatting. */}
-                    <img src="./images/play-circle.svg" alt="play" className="play-button" onClick="" role="button"/>
-                    <h1 className="mx-2 mb-0">{track.title}</h1>
-                    <p className="mb-0 song-length">({track.length})</p>
+            {/* This section handles search result slides */}
+            <div id="details-carousel" className="modal-body carousel slide">
+              <div className="carousel-indicators">
+                {song.tracks && song.tracks.map((track, index) => (
+                  <button type="button" data-bs-target="#details-carousel" data-bs-slide-to={index} className={index === 0 ? "active" : ""} aria-current={index === 0 ? "true" : ""} aria-label={`Search Result ${index+1}`} onClick={() => this.handleTrackIter(index)}></button>
+                ))}
+              </div>
+              <div className="carousel-inner">
+                {song.tracks && song.tracks.map((track, index) => (
+                  <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
+                    <div className="mb-2 details-header d-flex align-items-center">
+                      {/* This img is where the song preview play/pause button 
+                      should go. Still needs input, handler, and formatting. */}
+                      <img src="./images/play-circle.svg" alt="play" className="play-button" onClick="" role="button"/>
+                      <h1 className="mx-2 mb-0">{track.title}</h1>
+                      <p className="mb-0 song-length">({track.length})</p>
+                    </div>
+                    <p className="d-flex justify-content-between">
+                      <div>Artist:&nbsp;</div>
+                      <div>{track.artist}</div>
+                    </p>
+                    <p className="d-flex justify-content-between">
+                      <div>Album:&nbsp;</div>
+                      <div>{track.album}</div>
+                    </p>
                   </div>
-                  <p className="d-flex justify-content-between">
-                    <div>Artist:&nbsp;</div>
-                    <div>{track.artist}</div>
-                  </p>
-                  <p className="d-flex justify-content-between">
-                    <div>Album:&nbsp;</div>
-                    <div>{track.album}</div>
-                  </p>
-                </div>
-              ))}
+                ))}
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#details-carousel" data-bs-slide="prev" onClick={() => this.handleTrackIter("prev")}>
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#details-carousel" data-bs-slide="next" onClick={() => this.handleTrackIter("next")}>
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#details-carousel" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#details-carousel" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-          {/* End search result slides */}
+            {/* End search result slides */}
 
-          {/* These buttons need click handlers. The first button should
-          indicate that the currently displayed track is the one we want in
-          our playlist. The second button should remove the current song
-          and all of its tracks from the playlist entirely. */}
-          <div className="modal-footer d-flex">
-            <button type="button" className="btn btn-success flex-fill" data-bs-dismiss="modal">Confirm Selection</button>
-            <button type="button" className="btn btn-danger flex-fill">Remove Song</button>
+            <div className="modal-footer d-flex">
+              <button type="button" className="btn btn-success flex-fill" data-bs-dismiss="modal" onClick={() => this.handleConfirm()}>
+                Confirm Selection
+              </button>
+              <button type="button" className="btn btn-danger flex-fill" data-bs-dismiss="modal" onClick={() => this.props.updatePlaylist(null)}>
+                Remove Song
+              </button>
+            </div>
           </div>
         </div>
       </div>
