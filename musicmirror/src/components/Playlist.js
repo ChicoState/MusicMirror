a3d9d06285502c50620f4d9957e6972f3c9416d6
khasek <kmhasek@gmail.com>
2023-10-31 17:02:39 -0700

    disabled title change
    I was unable to fix the title change bugs in time, so the problematic code has been commented out. The title change functionality has been disabled, but everything else works. Also, I added a pencil icon.

--- musicmirror/src/components/Playlist.js
+++ musicmirror/src/components/Playlist.js
@@ -32,9 +32,9 @@ class Playlist extends React.Component{
   };
 
 
-  handleDoubleClick = () => {
+  handleDoubleClick = (two) => {
     console.log("handleDoubleClick");
-    if (!this.state.isEditing) {
+    if (two === 2) {
       this.setState({ isEditing: true }, () => {
         console.log("Now editing!");
       });
@@ -112,11 +112,17 @@ class Playlist extends React.Component{
         Object.keys(this.state.playlist).length > 0) {
       return (
         <div className="Playlist mt-3">
-          <h1>
+          <h1 className="d-flex justify-content-between">
             {/* {this.state.isEditing ? 
             (<input type="text" value={this.state.currentTitle} onChange={this.handleChange} onBlur={this.handleBlur} autoFocus/>) : 
-            (<span onDoubleClick={this.handleDoubleClick}>{this.state.currentTitle}</span>)} */}
-            {this.state.isEditing? (<span>Editing!</span>) : (<span onDoubleClick={this.handleDoubleClick}>Not Editing!</span>)}
+            (<span className="d-flex justify-content-between" onDoubleClick={this.handleDoubleClick}>{this.state.currentTitle}</span>)} */}
+            {this.state.currentTitle}
+            {/* Pencil icon: If we get the commented code above to work,
+            this should be moved inside the span. */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+            </svg>
           </h1>
 
           {/* List of song cards */}
