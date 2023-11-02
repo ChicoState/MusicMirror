e20d0dd27577806ae3f7b60c0d3329531bd0c035
khasek <kmhasek@gmail.com>
2023-11-01 19:20:07 -0700

    minor style changes
    pencil icon size is responsive, pencil icon does not disappear while editing, playlist title changes color when hovering over click area, cursor changes to text indicator when hovering over title edit click area

--- musicmirror/src/components/Playlist.js
+++ musicmirror/src/components/Playlist.js
@@ -126,21 +126,28 @@ class Playlist extends React.Component{
         Object.keys(this.state.playlist).length > 0) {
       return (
         <div className="Playlist mt-3">
-          <h1>
-            {this.state.isEditing ? 
-            (<input 
-              type="text" 
-              value={this.state.currentTitle} 
-              onChange={this.handleChange} 
-              onBlur={this.handleBlur} 
-              autoFocus
-            />) : 
+          <h1 className="list-title">
+            {this.state.isEditing? 
+            (<div className="d-flex justify-content-between align-items-center">
+              <input 
+                type="text" 
+                value={this.state.currentTitle} 
+                onChange={this.handleChange} 
+                onBlur={this.handleBlur} 
+                autoFocus
+              />
+              <svg className="bi bi-pencil-square" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>
+            </div>) 
+            : 
             (<span 
               className="d-flex justify-content-between align-items-center" 
               onDoubleClick={this.handleDoubleClick}
             >
               {this.state.currentTitle}
-              <svg className="bi bi-pencil-square" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <svg className="bi bi-pencil-square" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
