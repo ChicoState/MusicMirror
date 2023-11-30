3e3ca5cc91cb7e12b7ce22e0c80259d23d40a708
akachriss <chrisarobles27@gmail.com>
2023-11-29 18:47:39 -0800

    fix

--- musicmirror/src/auth.js
+++ musicmirror/src/auth.js
@@ -2,27 +2,32 @@ const clientId = "950b5e4bacb54dc1ad9c2ce70e2a4d48";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 let hasCode = false;
+let isTokenExchangeInProgress = false;
 
 export async function checkCode(){
     console.log("Ran checkCode");
     if (!code) {
-        redirectToAuthCodeFlow(clientId);
-        console.log("After everything code: " + code);
+        await redirectToAuthCodeFlow(clientId);
+        console.log("After redirectToAuthCodeFlow, code: " + code);
         hasCode = true;
     }
 };
 
 export async function signIn(loggedIn){
     console.log("code at signIn:" + code);
-    if(code){
-        console.log("In logged in");
+    //const bingo = params.get("code");
+    //console.log("bingo: ", bingo);
+    if(code && !localStorage.getItem("accessToken")){
+
+        const verifier = localStorage.getItem("verifier");
+        console.log("verifier before token request:", verifier);
+
         const accessToken = await getAccessToken(clientId, code);
-        if(!localStorage.getItem("token") && localStorage.getItem("token") !== "undefined"){
-            localStorage.setItem("token", accessToken);
+
+        if(accessToken && accessToken !== "undefined"){
+            const profile = await fetchProfile(accessToken);
+            populateUI(profile);
         }
-        // console.log("we out");
-        const profile = await fetchProfile(accessToken);
-        populateUI(profile);
         hasCode = true;
     }
 }
@@ -48,7 +53,6 @@ export async function redirectToAuthCodeFlow(clientId) {
 }
 
 export function generateCodeVerifier(length) {
-    console.log("generateCode");
     let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -67,9 +71,19 @@ export async function generateCodeChallenge(codeVerifier) {
         .replace(/\//g, '_')
         .replace(/=+$/, '');
 }
-
+/*
 export async function getAccessToken(clientId, code) {
+    console.log("access token here: ", localStorage.getItem("accessToken"));
+    if(localStorage.getItem("accessToken")){
+        console.log("HERE");
+        return localStorage.getItem("accessToken");
+    }
+    
     const verifier = localStorage.getItem("verifier");
+
+    console.log("verifier before token request: ", verifier);
+
+    console.log("Code before token request:", code);
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
@@ -78,25 +92,94 @@ export async function getAccessToken(clientId, code) {
     params.append("redirect_uri", "http://localhost:3000");
     params.append("code_verifier", verifier);
 
-    console.log("Get access token: " + code);
+    //console.log("Token request parameters:", params.toString());
+
+    //console.log("Code for access token: " + code);
     const result = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: params
     });
 
+    console.log("Token request result:", result);
+
+    if (!result.ok) {
+        const errorData = await result.json();
+        console.error("Token request error:", errorData);
+        // Handle the error case appropriately in your code
+        return null; // or throw an error
+    }
+
     const { access_token } = await result.json();
+    console.log("immediate at: ", access_token);
+
+    if(!localStorage.getItem("accessToken")){
+
+        localStorage.setItem("accessToken", access_token);
+    }
+    
     return access_token;
+}
+*/
+export async function getAccessToken(clientId, code) {
+    // Check if token exchange is already in progress
+    if (isTokenExchangeInProgress) {
+        console.log("Token exchange is already in progress. Ignoring this call.");
+        return null;
+    }
+
+    // Set the flag to indicate that token exchange is in progress
+    isTokenExchangeInProgress = true;
+
+    try {
+        const verifier = localStorage.getItem("verifier");
+
+        console.log("verifier before token request: ", verifier);
+        console.log("Code before token request:", code);
+
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("grant_type", "authorization_code");
+        params.append("code", code);
+        params.append("redirect_uri", "http://localhost:3000");
+        params.append("code_verifier", verifier);
+
+        console.log("Token request parameters:", params.toString());
+
+        const result = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: params
+        });
+
+        console.log("Token request result:", result);
+
+        if (!result.ok) {
+            const errorData = await result.json();
+            console.error("Token request error:", errorData);
+            // Handle the error case appropriately in your code
+            return null; // or throw an error
+        }
+
+        const { access_token } = await result.json();
+        console.log("Access token:", access_token);
+
+        localStorage.setItem("token", access_token);
+
+        return access_token;
+    } finally {
+        // Reset the flag when the token exchange is complete
+        isTokenExchangeInProgress = false;
+    }
 }
 
 export async function fetchProfile(token) {
-    console.log("token: " + token);
+    //console.log("token: " + token);
     
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
     let res = await result.json();
-    console.log("MFFRRPPHHH: " + token);
     console.log("Before user_id" + res.id);
     if(!localStorage.getItem("user_id")){
         localStorage.setItem("user_id", res.id);
