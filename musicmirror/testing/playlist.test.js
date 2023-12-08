d272e565c5d7e34693c8844a58ef9de4288329c7
Jhet Cabigas <cjhet02@gmail.com>
2023-12-08 14:56:48 -0800

    Added test for findSongs

--- musicmirror/testing/playlist.test.js
+++ musicmirror/testing/playlist.test.js
@@ -1,6 +1,48 @@
-//const playlist = require('../sum/playlist');
+import renderer from 'react-test-renderer';
+import { findSongs } from '../src/playlist';
+import fetchMock from "jest-fetch-mock";
+import 'whatwg-fetch'
 
-test('Smoke Test', () => {
-    expect(true).toBe(true);
+const responses = require('./testResponses.json');
+
+class sessionStorageMock {
+    constructor() {
+      this.store = {};
+    }
+  
+    clear() {
+      this.store = {};
+    }
+  
+    getItem(key) {
+      return this.store[key] || null;
+    }
+  
+    setItem(key, value) {
+      this.store[key] = String(value);
+    }
+  
+    removeItem(key) {
+      delete this.store[key];
+    }
+}
+  
+global.sessionStorage = new sessionStorageMock;
+
+beforeAll(() => {
+    fetchMock.enableMocks();
+});
+  
+beforeEach(() => {
+    fetch.resetMocks();
 });
 
+it('returns playlist object', async () => {
+    const message = 'test1';
+    fetch.mockResponses([
+        JSON.stringify(responses.spotify_resp)
+    ]);
+    sessionStorage.setItem("token", "test");
+    const resp = await findSongs(message, 5);
+    expect(JSON.stringify(resp)).toBe(JSON.stringify(responses.playlist));
+});
